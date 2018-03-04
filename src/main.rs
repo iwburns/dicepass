84753f1d5e85c5024f6d173b3707d6ca89da5365
Ian <iwburns8@gmail.com>
2018-03-04 16:37:07 -0600

    update to latest lib_dicepass_gen
    cleanup unused code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 extern crate clap;
 extern crate lib_dicepass_gen;
 
-//use std::io::prelude::*;
-//use std::fs::File;
-//use std::collections::HashMap;
-
 use clap::{Arg, ArgMatches, App};
-use lib_dicepass_gen::PassphraseGenerator;
+use lib_dicepass_gen::*;
 
 fn main() {
     let args = parse_args();
@@ -14,16 +10,15 @@ fn main() {
 //    let file_path = args.value_of("FILE").unwrap();
 //    let rolls_per_word: u32 = args.value_of("ROLLS").unwrap().parse().unwrap();
 
-    let pg = PassphraseGenerator::new();
-    let passphrase;
-
-    if let Some(num_words) = args.value_of("WORDS") {
-        passphrase = pg.generate_with_length(num_words.parse().unwrap());
+    let word_count = if let Some(num_words) = args.value_of("WORDS") {
+        WordCount::from(num_words.parse().ok())
     } else {
-        passphrase = pg.generate();
-    }
+        WordCount::Default
+    };
 
-    println!("{}", passphrase);
+    let pass_config = PassGenConfig::from_eff_short_2(word_count);
+
+    println!("{}", generate(pass_config));
 }
 
 fn parse_args<'a>() -> ArgMatches<'a> {
@@ -63,23 +58,3 @@ fn parse_args<'a>() -> ArgMatches<'a> {
             }))
         .get_matches()
 }
-//
-//fn parse_word_list(file_path: &str) -> std::io::Result<HashMap<String, String>> {
-//    let mut word_list = File::open(file_path)?;
-//
-//    let mut file_contents = String::new();
-//    word_list.read_to_string(&mut file_contents)?;
-//
-//    let line_count = file_contents.as_str().lines().count();
-//    let map = HashMap::with_capacity(line_count);
-//
-//    let map = file_contents.lines().fold(map, |mut acc, line| {
-//        let mut parts = line.split_whitespace();
-//        let key = parts.next().unwrap_or("").to_string();
-//        let value = parts.next().unwrap_or("").to_string();
-//        acc.insert(key, value);
-//        acc
-//    });
-//
-//    return Ok(map);
-//}
